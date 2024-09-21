a0cd08385d1a3dc451144fc074dc6071fab50b85
saway <sawa.y918@gmail.com>
2024-09-21 17:20:24 +0900

    initial commit:HelloWorld with SpringBoot

---
+++ src/main/java/reisetech/ClientManagement/ClientManagementApplication.java
@@ -0,0 +1,21 @@
+package reisetech.ClientManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ClientManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientManagementApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "nyan!";
+	}
+
+}
